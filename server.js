3a1f4630292cc43bf62b41597755a003989ab0aa
Shivam Gupta <shivamgupta019902@gmail.com>
2022-12-23 20:42:07 +0530

    committed

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
   console.log(req.files)
   let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
   res.redirect(`http://localhost:3000/static/${d}.pdf` )
-  // res.send({data: req.files})
-  // req.files is array of `photos` files
-  // req.body will contain the text fields, if there were any
+ 
 })
 
 app.listen(port, () => {
